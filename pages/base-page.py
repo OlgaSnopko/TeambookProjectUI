d0f0f6c9fd6463bf7130b4686dbf088d46e8eef0
OlgaSnopko <snopkoou@gmail.com>
2022-12-06 20:04:02 +0300

    Add Class BasePage

---
+++ pages/base-page.py
@@ -0,0 +1,8 @@
+class BasePage:
+    def __init__(self, browser, url, timeout=3):
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)
+
+    def open(self):
+        self.browser.get(self.url)
